1f765b4746e47c388a172d5331f0e41bf3e607d5
Richard Kanigoski <richardkanigoski@unerj.br>
2016-03-03 21:43:23 -0300

    Todos

---
+++ js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('menuCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('inicioCtrl', function($scope) {
+
+})
+ 
